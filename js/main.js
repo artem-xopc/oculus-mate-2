e496c5d69c097adeaec234e818089ec644358953
artem-xopc <romanovartem.2000@mail.ru>
2023-06-20 19:31:27 +0300

    added logic for switching blocks of the second section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
-$(function(){
+'use-strict';
 
+const tabBtn = document.querySelectorAll('.tabs__btn-item');
+const tabContent = document.querySelectorAll('.tabs__content-item');
+
+tabBtn.forEach(function (item) {
+  item.addEventListener('click', setOpen);
 });
+
+function setOpen(event) {
+  const tabTarget = event.currentTarget;
+  const button = tabTarget.dataset.button;
+
+  tabBtn.forEach(function (item) {
+    item.classList.remove('tabs__btn-item--active');
+  });
+  tabContent.forEach(function (item) {
+    item.classList.remove('tabs__content-item--active');
+  });
+
+  tabTarget.classList.add('tabs__btn-item--active');
+  document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
+}
